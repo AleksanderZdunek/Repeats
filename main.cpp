92dacdf77083f109786fd8ca786b9bbf9b82a20b
Aleksander Zdunek <halekun@gmail.com>
2018-02-05 17:16:36 +0100

    Milestone 1: First working prototype

--- main.cpp
+++ main.cpp
@@ -2,12 +2,23 @@
 	Find repeating substrings in an element stream.
 	Searches for repeating subtrings within a limited element window, or history buffer.
 */
+//compile: g++ -std=c++11 main.cpp
+//TODO: Makefile
+//TODO: Investigate window limits
+//Improvement: implement max distance between detected repeats?
+//Improvement: take advantage of that history buffer changes slowly, to not detect same repeat multiple times?
+//Improvement: use better string search algorithm?
 #include <iostream>
 #include <fstream>
 #include "circulararray_template.h"
 
 const char* filename = "alpha.txt";
 const size_t history_buffer_size = 16;
+
+const size_t min = 1;					//minimum length of repeat to detect
+const size_t max = history_buffer_size;	//maximum length of repeat to detect
+
+int DetectRepeats(CircularArray<char>& buffer);
 
 int main(int argc, char* argv[])
 {
@@ -23,10 +34,46 @@ int main(int argc, char* argv[])
 		history.PushBack(c);
 		for(int i=0; i<history.Size(); i++)
 		{
-			std::cout<<history[i];
+			DetectRepeats(history);
 		}
-		std::cout << std::endl;
 	}
 	
 	return 0;
+}
+
+bool StringCompare(const CircularArray<char>& buffer, const size_t s1, const size_t s2, size_t length)
+{
+	for(int i=0; i<length; i++) if(buffer[s1+i] != buffer[s2+i]) return false;
+	return true;
+}
+
+bool FindString(const CircularArray<char>& buffer, const size_t patternStartIndex, const size_t patternEndIndex, const size_t stringStartIndex, const size_t stringEndIndex)
+{
+	for(size_t i=stringStartIndex; i<stringEndIndex+patternStartIndex-patternEndIndex; i++)
+	{
+		if(StringCompare(buffer, patternStartIndex, i, patternEndIndex-patternStartIndex+1)) return true;
+	}
+	return false;
+}
+
+int DetectRepeats(CircularArray<char>& buffer)
+{
+	int count = 0;
+	
+	const size_t length = buffer.Size();
+	for(int i=0; i<length-min; i++)
+	{
+		for(int j=i+min-1; j<std::min((length+i)/2, i+max); j++)
+		{
+			if(FindString(buffer, i, j, j+1, length-1))
+			{
+				count++;
+				//print detected pattern
+				for(size_t k=i; k<=j; k++) std::cout<<buffer[k];
+				std::cout << std::endl;
+			};
+		}
+	}
+	
+	return count;
 }
