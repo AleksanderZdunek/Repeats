b84578483d48338f6720d9541536b769227defa1
Aleksander Zdunek <halekun@gmail.com>
2018-02-07 17:35:36 +0100

    Loop detection and detection reporting printout. Detection reporting code is nauseating and candidate for improvement.

--- Loops/main.cpp
+++ Loops/main.cpp
@@ -1,26 +1,73 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 #include "circulararray_template.h"
 
 const char* filename = "test.txt";
 const size_t history_buffer_size = 16;
 
+struct LoopInfo
+{
+	uint64_t streamPosition; //	1-indexed. The first element in the stream should have position 1.
+	size_t loopCount;
+	size_t loopSize;
+};
+std::ostream& operator<<(std::ostream& os, const LoopInfo& info)  
+{
+	os << "Loop @ stream pos: " << info.streamPosition << "\nSize: " << info.loopSize << "\nCount: " << info.loopCount << "\n";
+	return os;
+} 
+
 std::string toString(const CircularArray<char>& buffer, size_t startIndex);
-bool DetectLoop(const CircularArray<char>& buffer);
+size_t DetectLoop(const CircularArray<char>& buffer);
 
 int main(int argc, char* argv[])
 {
 	std::cout <<  "Loop detection" << std::endl;
 	
+	uint64_t streamCount(0);
+	LoopInfo loopInfo{0,0,0};
 	std::ifstream file(filename);
 	CircularArray<char> history(history_buffer_size);
 	
+	size_t detect;
 	char c;
 	while(file >> c)
 	{
+		++streamCount;
+		
 		if(history.Full()) history.PopBack();
 		history.PushFront(c);
-		DetectLoop(history);
+		
+		//Detection report print. //Note: this code looks like it wants to be a state machine.
+		if(detect = DetectLoop(history))
+		{
+			if(detect == loopInfo.loopSize)
+			{
+				loopInfo.loopCount++;
+			}
+			else //detected loop size has changed
+			{
+				if(loopInfo.loopSize) std::cout << loopInfo << "\n"; //print out info for previous loop size
+				loopInfo.loopSize = detect;
+				loopInfo.streamPosition = streamCount - loopInfo.loopSize;
+				loopInfo.loopCount = 1;
+			}
+		}
+		else
+		{
+			if(loopInfo.loopSize)
+			{
+				std::cout << loopInfo << "\n"; //print loop info
+				loopInfo.loopCount = loopInfo.loopSize = 0;
+			}
+		}
+	}
+	//Print out if stream happens to end on a loop
+	if(loopInfo.loopSize)
+	{
+		std::cout << loopInfo << "\n"; //print loop info
+		loopInfo.loopCount = loopInfo.loopSize = 0;//superflous here
 	}
 	
 	return 0;
@@ -32,7 +79,13 @@ bool StringCompare(const CircularArray<char>& buffer, const size_t s1, const siz
 	return true;
 }
 
-bool DetectLoop(const CircularArray<char>& buffer)
+/**
+	Examines the front of the buffer for looping substring
+	
+	\param buffer 
+	\return Length (nr of character) of detected loop. 0 if no loop detected.
+*/
+size_t DetectLoop(const CircularArray<char>& buffer)
 {
 	const size_t length = buffer.Size();
 	for(size_t i=0; i < length/2; i++)
@@ -40,10 +93,10 @@ bool DetectLoop(const CircularArray<char>& buffer)
 		if(StringCompare(buffer, 0, i+1, i+1))
 		{
 			std::cout << toString(buffer, i) << "\n";
-			return true;
+			return i+1;
 		}
 	}
-	return false;
+	return 0;
 }
 
 std::string toString(const CircularArray<char>& buffer, size_t startIndex)
